9eb2cb52292f103524194df77700ba9d117e6fbd
Ramnath <ramnath2609@gmail.com>
2020-08-03 22:15:11 +0530

    ajkfhkj

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from "react";
-import { createGlobalStyle } from "styled-components";
 import Appbar from "./components/Appbar";
 import SideBar from "./components/SideBar";
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyles />
       <Appbar />
       <SideBar />
     </div>
   );
 }
 
-const GlobalStyles = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap');
-  
-  
-  body {
-    font-family: 'Roboto', sans-serif;
-    margin: 0px;
-  }
-`;
+// const GlobalStyles = createGlobalStyle`
+//   @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap');
+
+//   body {
+//     font-family: 'Roboto', sans-serif;
+//     margin: 0px;
+//   }
+// `;
 
 export default App;
